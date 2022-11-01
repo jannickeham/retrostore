2e8101534058e279c668b29c40b267a339614702
jannickeham <jannicke.hamarsnes@gmail.com>
2022-11-01 13:54:17 +0100

    implement routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,46 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import DetailPage from "../views/DetailPage.vue";
+import AuthenticationPage from "../views/AuthenticationPage.vue";
+import ProfilePage from "../views/ProfilePage.vue";
+import PageNotFoundPage from "../views/PageNotFoundPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
-]
+    path: "/home",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/detail/:id",
+    name: "Detail",
+    component: DetailPage,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: AuthenticationPage,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: ProfilePage,
+  },
+  {
+    path: "/:catchAll(.*)*",
+    name: "PageNotFound",
+    component: PageNotFoundPage,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/PageNotFoundPage.vue
@@ -0,0 +1,2 @@
+<script setup lang="ts"></script>
+<template><div>404 not found</div></template>
