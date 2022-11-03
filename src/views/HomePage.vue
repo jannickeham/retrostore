94046dde6296142d5ad812fabdb1872a3c9c8e16
jannickeham <jannicke.hamarsnes@gmail.com>
2022-11-03 13:42:18 +0100

    avatar and profile name

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -53,13 +53,6 @@ const productCardInfo = ref([
     <ion-header :translucent="true">
       <ion-toolbar class="toolbar">
         <ion-title>RetroStore</ion-title>
-        <ion-button
-          slot="end"
-          shape="round"
-          color="bgm"
-          router-link="/new-product"
-          >+</ion-button
-        >
       </ion-toolbar>
     </ion-header>
 
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,2 +1,51 @@
-<script setup lang="ts"></script>
-<template><div>Profilepage</div></template>
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonAvatar,
+} from "@ionic/vue";
+</script>
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar class="toolbar">
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/home"></ion-back-button>
+        </ion-buttons>
+        <ion-title>Min profil</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <div class="profile-container">
+        <ion-avatar class="profile-avatar">
+          <img
+            alt="Profil picture"
+            src="https://ionicframework.com/docs/img/demos/avatar.svg"
+          />
+        </ion-avatar>
+        <h1 class="profile-name">Navn Navnesen</h1>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<style>
+.profile-avatar {
+  width: 10rem !important;
+  height: auto !important;
+}
+
+.profile-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+}
+</style>
