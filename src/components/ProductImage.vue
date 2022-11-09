ce1809d7b403772bc072b018b6d4f9fdd6e791ec
jannickeham <jannicke.hamarsnes@gmail.com>
2022-11-09 20:45:14 +0100

    removed access_token from image tag

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -9,7 +9,5 @@ defineProps<Props>();
 </script>
 
 <template>
-  <img
-    :src="`https://v6a8qmt5.directus.app/assets/${imageId}?access_token=${userAccessToken}`"
-  />
+  <img :src="`https://v6a8qmt5.directus.app/assets/${imageId}`" />
 </template>
